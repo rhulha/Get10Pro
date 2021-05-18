c538d36f568255309450face3e557d12529c15f1
Ray <java4life@gmail.com>
2021-05-18 06:20:48 +0200

    Still trying to make the game work offline on mobiles. This time trying to use a Serive Worker: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'get10pro-v1'
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(['index.html'])));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) return r;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
+
+/*
+self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
+*/
